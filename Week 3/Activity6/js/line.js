7cae3d29e0ca1cfff438568273db67f8df891011
sydnideeventer <30667957+sydnideeventer@users.noreply.github.com>
2022-02-06 19:07:44 -0500

    Add files via upload

---
+++ Week 3/Activity6/js/line.js
@@ -0,0 +1,38 @@
+class Line {
+
+  constructor(_config) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: _config.containerWidth || 500,
+      containerHeight: _config.containerHeight || 140,
+      margin: { top: 10, bottom: 30, right: 10, left: 30 }
+    }
+
+    // Call a class function
+    this.initVis();
+  }
+
+  initVis() {
+      
+
+
+      updateVis(); //leave this empty for now...
+  }
+
+
+  //leave this empty for now
+ updateVis() { 
+   
+   renderVis(); 
+
+ }
+
+
+ //leave this empty for now...
+ renderVis() { 
+
+  }
+
+
+
+}
